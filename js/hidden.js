66b44a662be7bf3614edbb40d69713d8fb1f448a
arsa994 <arsic.milos30@gmail.com>
2018-11-18 20:47:18 +0100

    first commit

---
+++ js/hidden.js
@@ -0,0 +1,5 @@
+const a = document.querySelector(".menu-vertical");
+  
+document.querySelector('.menu-vertical').addEventListener('click', function(){
+  const b = document.querySelector(".hidden").className="menu-vertical";
+});
