573a3cda94b35eac2e7eec6893cffc3b1838c797
arsa994 <arsic.milos30@gmail.com>
2018-11-19 15:31:21 +0100

    html commit

--- js/hidden.js
+++ js/hidden.js
@@ -1,5 +1,8 @@
-const a = document.querySelector(".menu-vertical");
-  
-document.querySelector('.menu-vertical').addEventListener('click', function(){
-  const b = document.querySelector(".hidden").className="menu-vertical";
+$('.input').change(function()
+{
+  if($(this).attr('value') == "0") {
+    $('#choose_own_text').append('<input type="text" id="my_own_text" name="my_own_text" value="Please type in .." />');
+  } else {
+    $('#choose_own_text').empty();
+  }
 });
